15bb7877fd9a342719d11ef2bee9028f008772ac
bitshit <mmaaddii93@gmail.com>
2013-04-09 10:28:52 +0200

    doing shit with snake

--- src/base/snake.c
+++ src/base/snake.c
@@ -15,6 +15,12 @@
 // load snake
 void loadSnake( void )
 {
+  
+ unsigned char Pos_X;
+ unsigned char Pos_Y;
+ unsigned char Pos_X_Hinten;
+ unsigned char Pos_Y_Hinten;
+     
 
 	// set up screen
 	cls();
@@ -26,4 +32,26 @@ void loadSnake( void )
 void processSnake( void )
 {
 	FrameWork.state = FRAMEWORK_STATE_LOAD_MENU;
+        
+        if (player1BottomLeft())
+          Pos_X = Pos_X-1;
+        
+        if (player1BottomRight())
+          Pos_X = Pos_X+1;
+        
+        if (player1BottomUp())
+          Pos_X = Pos_Y+1;        
+
+        if (player1BottomDown())
+          Pos_X = Pos_Y-1; 
+
+   
+        
+        dot(Pos_X, Pos_Y, 0x00E)
+        dot(Pos_X_Hinten, Pos_Y_Hinten, 0) 
+        
+        
+        
+        
+        
 }
